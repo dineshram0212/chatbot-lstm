915dd6f530efa6bdd819d9019842486c1a010838
Dinesh Ram R <dineshram0212@gmail.com>
2021-07-29 08:53:09 +0530

    upto creating train data

--- chatbot_train.py
+++ chatbot_train.py
@@ -1,12 +1,12 @@
+############### Imports ###############
+
 import nltk
 from nltk import data
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-nltk.download('punkt')
-# nltk.download('')
-
+###############
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
@@ -68,4 +68,29 @@ training = []
 # Create an empty array for the output
 output_empty = [0] * len(classes)
 
-# 
+# training set --> bag of words for each sentence
+for doc in documents:
+    # initialize our bag of words
+    bag = []
+    # list of tokenize words for patterns
+    pattern_words = doc[0]
+    # lemmatize each word - Create base word, in attempt to represent related words
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    # Create our bag of words array with 1, if word match found in current pattern
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output is '0' for each tag and '1' for current tag(for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# Shuffle Our Features and Turn into Array
+random.shuffle(training)
+training = np.array(training)
+
+# Create Train and Test lists : X - patterns, y - intents
+x_train = list(training[:,0])
+y_train = list(training[:,1])
+print("--Training Data Created--")
